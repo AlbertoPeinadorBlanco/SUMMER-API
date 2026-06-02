c39ffec68323e0b1389462b33778cbe8c04b1cf4
Alberto Peinador Blanco <Albertopeinadorblanco@gmail.com>
2026-06-02 08:18:37 +0200

    test: update mailer utility to trigger verification email instead of support email

--- test-mailer.js
+++ test-mailer.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
-const { sendSupportEmail } = require('./src/utils/mailer');
+const { sendVerificationEmail } = require('./src/utils/mailer');
 
 async function test() {
     try {
-        await sendSupportEmail('Test', 'test@example.com', 'Test Subject', 'Test Message');
+        await sendVerificationEmail('surfmarket.contact@gmail.com', 'test-verification-token-12345');
         console.log('Success');
     } catch (e) {
         console.error('Error:', e);
