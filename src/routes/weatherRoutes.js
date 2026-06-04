6378f5243d12f2029c091d417b5603d03fbb3c2a
Alberto Peinador Blanco <Albertopeinadorblanco@gmail.com>
2026-06-04 07:02:07 +0200

    feat: add /live-conditions weather route to expose live conditions endpoint

--- src/routes/weatherRoutes.js
+++ src/routes/weatherRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const weatherController = require('../controllers/weatherController');
-
+//weather
 router.get('/live-conditions', weatherController.getLiveConditions);
 //export
 module.exports = router;
